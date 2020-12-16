80518e80e72a7c90fc1221d98b86dc986011bfbb
Anirban Banerjee <Anirban Banerjee>
2020-12-17 00:46:38 +0530

    changed home page directory view

--- src/redux/directory/directory.reducer.js
+++ src/redux/directory/directory.reducer.js
@@ -2,32 +2,32 @@ const INITIAL_STATE = {
   sections: [
     {
       title: 'hats',
-      imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
+      imageUrl: 'https://www.brixton.com/pub/media/catalog/category/HO19-GIFT-GUIDE-CLP-HAT-SHOP.jpg',//'https://i.ibb.co/cvpntL1/hats.png',
       id: 1,
       linkUrl: 'shop/hats'
     },
     {
       title: 'jackets',
-      imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
+      imageUrl: 'https://c1.wallpaperflare.com/preview/482/162/421/jackets-exhibition-shop-shopping.jpg',//'https://i.ibb.co/px2tCc3/jackets.png',
       id: 2,
       linkUrl: 'shop/jackets'
     },
     {
       title: 'sneakers',
-      imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
+      imageUrl: 'https://i.pinimg.com/originals/53/bc/f0/53bcf092484cdf9e92e53b1dcaaecbbd.jpg',//'https://i.ibb.co/0jqHpnp/sneakers.png',
       id: 3,
       linkUrl: 'shop/sneakers'
     },
     {
       title: 'womens',
-      imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
+      imageUrl: 'https://www.newsbite.org/wp-content/uploads/2019/01/ladies.png',//'https://www.coverstory.co.in/media/cms/lookbook/lookbook-9/menu1.jpg',//'https://i.ibb.co/GCCdy8t/womens.png',
       size: 'large',
       id: 4,
       linkUrl: 'shop/womens'
     },
     {
       title: 'mens',
-      imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
+      imageUrl: 'https://www.bosshunting.com.au/wp-content/uploads/2020/03/academy-brand-1027x560.jpg',//'https://i.ibb.co/R70vBrQ/men.png',
       size: 'large',
       id: 5,
       linkUrl: 'shop/mens'
