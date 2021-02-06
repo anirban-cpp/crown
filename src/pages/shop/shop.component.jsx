b14bcbe5f6b988bf5d799409310e0b9d01011c14
Anirban Banerjee <Anirban Banerjee>
2021-02-06 16:51:39 +0530

    implement code that pulls in our shop data from firestore and stores it into the shop reducer from our shop page

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -21,10 +21,25 @@ class ShopPage extends React.Component{
   componentDidMount(){
     const { updateCollections } =this.props;
     const collectionRef = store.collection('collection');
+    // fetch() method to get the data from firestore. We are not using this as the data is quited nested here.
+
+    /*fetch('https://firestore.googleapis.com/v1/projects/crwn-db-a10f1/databases/(default)/documents/collection')
+    .then(response => response.json())
+    .then(collection => console.log(collection));*/
+
+    // Promise based data fetch using get() which makes an API call to get the data associated with this collectionRef
+    // to handle async data fetch
+
+    /*collectionRef.get().then(snapshot => {
+      const collectionsMap = convertCollectionSnapshotToMap(snapshot);
+      updateCollections(collectionsMap);
+      this.setState({ loading: false });
+    })*/
+
+    //  Observable based data fetch
 
     this.unscubscribeFromSnapshot = collectionRef.onSnapshot(async snapshot => {
       const collectionsMap = convertCollectionSnapshotToMap(snapshot);
-      console.log(collectionsMap);
       updateCollections(collectionsMap);
       this.setState({ loading: false });
     });
