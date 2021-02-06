758c16f6f12638572a7b5549525435e813ca3f4e
Anirban Banerjee <Anirban Banerjee>
2021-02-06 19:42:33 +0530

    moved isLoading and isFetching to their own components

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -2,25 +2,22 @@ import React from 'react';
 import { Route } from 'react-router-dom';
 import { connect } from 'react-redux';
 
-import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
-import CollectionPage from '../collection/collection.component';
-import { store, convertCollectionSnapshotToMap } from '../../firebase/firebase.utils';
-import  { updateCollections } from '../../redux/shop/shop.actions';
-import WithSpinner from '../../components/with-spinner/with-spinner.component';
+import { fetchCollectionsStartAsync } from '../../redux/shop/shop.actions';
+import { selectIsCollectionLoaded } from '../../redux/shop/shop.selectors';
 
-const CollectionOverviewWithSpinner = WithSpinner(CollectionsOverview);
-const CollectionPageWithSpinner = WithSpinner(CollectionPage);
+import CollectionsOverviewContainer from '../../components/collections-overview/collections-overview.container';
+import CollectionPageContainer from '../collection/collection.container';
 
 class ShopPage extends React.Component{
-  state = {
+/*  state = {
     loading: true
   };
 
   unscubscribeFromSnapshot = null;
 
   componentDidMount(){
-    const { updateCollections } =this.props;
-    const collectionRef = store.collection('collection');
+  //  const { updateCollections } =this.props;
+  //  const collectionRef = store.collection('collection');
     // fetch() method to get the data from firestore. We are not using this as the data is quited nested here.
 
     /*fetch('https://firestore.googleapis.com/v1/projects/crwn-db-a10f1/databases/(default)/documents/collection')
@@ -38,28 +35,32 @@ class ShopPage extends React.Component{
 
     //  Observable based data fetch
 
-    this.unscubscribeFromSnapshot = collectionRef.onSnapshot(async snapshot => {
+   /* this.unscubscribeFromSnapshot = collectionRef.onSnapshot(async snapshot => {
       const collectionsMap = convertCollectionSnapshotToMap(snapshot);
       updateCollections(collectionsMap);
       this.setState({ loading: false });
-    });
+    });*/
+
+  //}
+
+  componentDidMount() {
+    const { fetchCollectionsStartAsync } = this.props;
+    fetchCollectionsStartAsync();
   }
 
   render(){
-    const {match} = this.props;
-    const { loading } = this.state;
+    const { match } = this.props;
     return(
       <div className='shop-page'>
-        <Route exact path={`${match.path}`} render={(props) => <CollectionOverviewWithSpinner isLoading={loading} {...props} />} />
-        <Route path={`${match.path}/:collectionId`} render={(props) => <CollectionPageWithSpinner isLoading={loading} {...props} />} />
+        <Route exact path={`${match.path}`} component={CollectionsOverviewContainer} />
+        <Route path={`${match.path}/:collectionId`} component={CollectionPageContainer} />
       </div>
     );
   }
 }
 
 const mapDispatchToProps = dispatch => ({
-  updateCollections: collectionsMap =>
-    dispatch(updateCollections(collectionsMap))
+  fetchCollectionsStartAsync: () => dispatch(fetchCollectionsStartAsync())
 });
 
 export default connect(null, mapDispatchToProps)(ShopPage);
